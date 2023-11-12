0aa8ae8d26bcecd273feca461d4c4c8f1ec19302
Sergei-vu <150508987+Sergei-vu@users.noreply.github.com>
2023-11-12 17:34:47 +0300

    Update blog-post-archive.js

--- src/templates/blog-post-archive.js
+++ src/templates/blog-post-archive.js
@@ -5,6 +5,9 @@ import parse from "html-react-parser"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { AudioPlayerProvider } from '../components/AudioPlayerContext';
+import AudioPlayer from '../components/AudioPlayer';
+
 
 const BlogIndex = ({
   data,
